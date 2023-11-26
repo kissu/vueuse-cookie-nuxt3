2ffb8e43d7f047cfd78c51360d1184c2a5a5d652
Konstantin BIFERT <konstantin.bifert@gmail.com>
2023-11-26 18:32:16 +0100

    done

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <strong>locale</strong>: {{ cookies.get('locale') }}
+    <hr>
+    <pre>{{ cookies.getAll() }}</pre>
+    <button @click="cookies.set('locale', 'ru-RU')">Russian</button>
+    <button @click="cookies.set('locale', 'en-US')">English</button>
+  </div>
+</template>
+
+<script setup>
+import { useCookies } from '@vueuse/integrations/useCookies'
+
+const cookies = useCookies(['locale'])
+</script>
